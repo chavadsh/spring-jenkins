e555b030dbc28cffea22d0a64f3613abf0bd5ca3
shivaraj c <schavadappanavar@gmail.com>
2022-05-15 21:16:56 +0900

    first commit

---
+++ spring-jenkins/src/test/java/com/chavadsh/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.chavadsh.springjenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	
+	@Test
+	void contextLoads() {
+		logger.info("Executing test case");
+		assertEquals(true, true);
+	}
+
+}
